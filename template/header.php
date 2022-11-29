9ef95bafe4754679409803f009095894cc6dd33d
ALonelyKing89 <119292039+ALonelyKing89@users.noreply.github.com>
2022-11-29 12:53:58 +0500

    Update header.php

--- template/header.php
+++ template/header.php
@@ -6,19 +6,14 @@
 	<title>Document</title>
 </head>
 <body>
-<div class="container">
-    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-      <div class="px-3 py-2 border-bottom mb-3">
-      <div class="container d-flex flex-wrap justify-content-center">
-        <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
-          <input type="search" class="form-control" placeholder="Поиск..." aria-label="Search">
-        </form>
 
-        <div class="text-end">
-          <button type="button" class="btn btn-light text-dark me-2"><font style="vertical-align: inherit;">Авторизоваться </font></button>
-          <button type="button" class="btn btn-primary"><font style="vertical-align: inherit;">Подписаться </font></button>
-        </div>
-      </div>
-    </div>
-    </header>
-  </div>	
+    <nav class="navbar bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand">Navbar</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+

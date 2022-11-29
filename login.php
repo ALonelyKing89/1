a3895fac12a2fd060eb99aaae8bed9ee1b20b573
zabolotnevright <53403318+Lunalord666@users.noreply.github.com>
2022-11-29 11:00:30 +0500

    asd

--- login.php
+++ login.php
@@ -52,26 +52,20 @@ if (isset($_POST["doGo"])) {
 <body>
 
     <main class="form-signin w-100 m-auto">
-        <form>
-            <img class="mb-4" src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-            <h1 class="h3 mb-3 fw-normal">Авторизация</h1>
+        <form class="container">
+            <img class="mx-auto my-4 d-flex col-1" src="logo.png">
+            <h1 class="h3 my-5 fw-normal text-center">Авторизация</h1>
 
-            <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                <label for="floatingInput">Email address</label>
+            <div class="form-floating col-4 my-4 mx-auto">
+                <input  type="email" class="form-control" id="floatingInput" placeholder="login" name="login">
+                <label class="text-center"  for="floatingInput">login</label>
             </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                <label for="floatingPassword">Password</label>
+            <div class="form-floating col-4 my-4 mx-auto">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="password" name="pass">
+                <label  for="floatingPassword">password</label>
             </div>
 
-            <div class="checkbox mb-3">
-                <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                </label>
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-            <p class="mt-5 mb-3 text-muted">© 2017–2022</p>
+            <button class=" btn btn-lg btn-primary my-4 col-4 mx-auto d-flex" type="submit">Войти</button>
         </form>
     </main>
 

85f6bd83f2a2e5bf2d973390196432e4c5e7554f
zabolotnevright <53403318+Lunalord666@users.noreply.github.com>
2022-11-29 12:27:20 +0500

    baza

--- hakaton2022.sql
+++ hakaton2022.sql
@@ -1,109 +1,77 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Ноя 29 2022 г., 10:19
--- Версия сервера: 5.7.33
--- Версия PHP: 7.4.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Хост:                         127.0.0.1
+-- Версия сервера:               8.0.24 - MySQL Community Server - GPL
+-- Операционная система:         Win64
+-- HeidiSQL Версия:              11.3.0.6295
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- База данных: `hakaton2022`
---
 
--- --------------------------------------------------------
+-- Дамп структуры базы данных hakaton2022
+CREATE DATABASE IF NOT EXISTS `hakaton2022` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hakaton2022`;
 
---
--- Структура таблицы `category`
---
+-- Дамп структуры для таблица hakaton2022.category
+CREATE TABLE IF NOT EXISTS `category` (
+  `id_category` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id_category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
-CREATE TABLE `category` (
-  `id_category` int(11) NOT NULL,
-  `name` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Дамп данных таблицы hakaton2022.category: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
 
--- --------------------------------------------------------
+-- Дамп структуры для таблица hakaton2022.id_foto
+CREATE TABLE IF NOT EXISTS `id_foto` (
+  `id_foto` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id_foto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
---
--- Структура таблицы `id_foto`
---
+-- Дамп данных таблицы hakaton2022.id_foto: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `id_foto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `id_foto` ENABLE KEYS */;
 
-CREATE TABLE `id_foto` (
-  `id_foto` int(11) NOT NULL,
-  `name` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `problem`
---
-
-CREATE TABLE `problem` (
-  `id_problem` int(5) NOT NULL,
+-- Дамп структуры для таблица hakaton2022.problem
+CREATE TABLE IF NOT EXISTS `problem` (
+  `id_problem` int NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `text` text NOT NULL,
-  `id_category` int(5) NOT NULL,
-  `id_foto` int(11) NOT NULL,
+  `id_category` int NOT NULL,
+  `id_foto` int NOT NULL,
   `status` varchar(15) NOT NULL DEFAULT 'новая',
-  `time` timestamp NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `time` timestamp NOT NULL,
+  PRIMARY KEY (`id_problem`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
---
--- Индексы сохранённых таблиц
---
+-- Дамп данных таблицы hakaton2022.problem: ~0 rows (приблизительно)
+/*!40000 ALTER TABLE `problem` DISABLE KEYS */;
+/*!40000 ALTER TABLE `problem` ENABLE KEYS */;
 
---
--- Индексы таблицы `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id_category`);
+-- Дамп структуры для таблица hakaton2022.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id_user` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
 
---
--- Индексы таблицы `id_foto`
---
-ALTER TABLE `id_foto`
-  ADD PRIMARY KEY (`id_foto`);
+-- Дамп данных таблицы hakaton2022.users: ~3 rows (приблизительно)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id_user`, `login`, `password`) VALUES
+	(1, 'admin', 'admin'),
+	(2, 'ivan', 'zabi'),
+	(7, 'abobi', '123');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
---
--- Индексы таблицы `problem`
---
-ALTER TABLE `problem`
-  ADD PRIMARY KEY (`id_problem`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `category`
---
-ALTER TABLE `category`
-  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `id_foto`
---
-ALTER TABLE `id_foto`
-  MODIFY `id_foto` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `problem`
---
-ALTER TABLE `problem`
-  MODIFY `id_problem` int(5) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;

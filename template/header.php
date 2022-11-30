f8df65f9f6a77a72456b1a1f78d6a1d20d4643a5
Kloody <kloodyjpe@gmail.com>
2022-12-01 02:06:53 +0500

    fdfd

--- template/header.php
+++ template/header.php
@@ -11,38 +11,18 @@
 
   ?>
 
-<<<<<<< Updated upstream
-  <nav class="navbar sticky-top badge-light bg-light">
-    <div class="container">
-      <?
-      if (isset($_SESSION["login"])) {
-        echo '<a class="navbar-brand" href="\">
-        <img src="logo.png" alt="" width="30">
-      </a>';
-      } else {
-        echo '<a class="navbar-brand">
-        <img src="logo.png" alt="" width="30">
-      </a>';
-      }
-      ?>
-=======
   <nav class="navbar sticky-top badge-light">
     <div class="container">
       <a class="navbar-brand" href="\">
         <img src="logo.png" alt="" width="40">
       </a>
->>>>>>> Stashed changes
       <form class="d-flex btn-group" role="search">
         <?
         if (isset($_SESSION["login"])) {
           if ($_SESSION["adm"] == 1) {
             echo '<a href="Acabinet.php" class="btn btn-info text-white">Админка</a>';
           } else {
-<<<<<<< Updated upstream
             echo '<a href="Ucabinet.php" class="btn btn-info text-white">Профиль</a>';
-=======
-            echo '<a href="Ucabinet.php" class="btn btn-info text-white">Личный кабинет</a>';
->>>>>>> Stashed changes
           }
           echo '<a href="login.php" class="btn btn-danger">Выход</a>';
         } else {

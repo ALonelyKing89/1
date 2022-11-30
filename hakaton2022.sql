c0e494b0099fc4db42fc9fef05335cc4273c1bc1
zabolotnevright <53403318+Lunalord666@users.noreply.github.com>
2022-11-30 19:05:03 +0500

    baza

--- hakaton2022.sql
+++ hakaton2022.sql
@@ -1,103 +1,221 @@
--- --------------------------------------------------------
--- Хост:                         127.0.0.1
--- Версия сервера:               8.0.24 - MySQL Community Server - GPL
--- Операционная система:         Win64
--- HeidiSQL Версия:              11.3.0.6376
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Ноя 30 2022 г., 17:04
+-- Версия сервера: 8.0.24
+-- Версия PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- База данных: `hakaton2022`
+--
 
--- Дамп структуры базы данных hakaton2022
-CREATE DATABASE IF NOT EXISTS `hakaton2022` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `hakaton2022`;
+-- --------------------------------------------------------
 
--- Дамп структуры для таблица hakaton2022.category
-CREATE TABLE IF NOT EXISTS `category` (
-  `id_category` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  PRIMARY KEY (`id_category`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+--
+-- Структура таблицы `category`
+--
 
--- Дамп данных таблицы hakaton2022.category: ~2 rows (приблизительно)
+CREATE TABLE `category` (
+  `id_category` int NOT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп данных таблицы `category`
+--
+
 INSERT INTO `category` (`id_category`, `name`) VALUES
-	(1, 'Problem 1'),
-	(2, 'Problem 2');
+(1, 'Problem 1'),
+(2, 'Problem 2');
 
--- Дамп структуры для таблица hakaton2022.photo
-CREATE TABLE IF NOT EXISTS `photo` (
-  `id_photo` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_photo`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+-- --------------------------------------------------------
 
--- Дамп данных таблицы hakaton2022.photo: ~1 rows (приблизительно)
+--
+-- Структура таблицы `photo`
+--
+
+CREATE TABLE `photo` (
+  `id_photo` int NOT NULL,
+  `name` varchar(50) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Дамп данных таблицы `photo`
+--
+
 INSERT INTO `photo` (`id_photo`, `name`) VALUES
-	(1, 'asdfasfh');
+(1, 'asdfasfh');
 
--- Дамп структуры для таблица hakaton2022.problem
-CREATE TABLE IF NOT EXISTS `problem` (
-  `id_problem` int NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `problem`
+--
+
+CREATE TABLE `problem` (
+  `id_problem` int NOT NULL,
   `name` varchar(20) NOT NULL,
   `text` text NOT NULL,
   `id_category` int NOT NULL,
   `id_photo` int NOT NULL,
   `status` varchar(15) NOT NULL DEFAULT 'новая',
   `time` timestamp NULL DEFAULT NULL,
-  `id_user` int DEFAULT NULL,
-  PRIMARY KEY (`id_problem`),
-  KEY `FK_problem_photo` (`id_photo`),
-  KEY `FK_problem_category` (`id_category`),
-  KEY `FK_problem_users` (`id_user`),
-  CONSTRAINT `FK_problem_category` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_problem_photo` FOREIGN KEY (`id_photo`) REFERENCES `photo` (`id_photo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_problem_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+  `id_user` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Дамп данных таблицы hakaton2022.problem: ~1 rows (приблизительно)
+--
+-- Дамп данных таблицы `problem`
+--
+
 INSERT INTO `problem` (`id_problem`, `name`, `text`, `id_category`, `id_photo`, `status`, `time`, `id_user`) VALUES
-	(1, 'asd', 'asd', 2, 1, 'новая', '2022-11-30 13:53:23', 7);
+(1, 'asd', 'asd', 2, 1, 'новая', '2022-11-30 13:53:23', 7);
 
--- Дамп структуры для таблица hakaton2022.request
-CREATE TABLE IF NOT EXISTS `request` (
-  `id_request` int NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `request`
+--
+
+CREATE TABLE `request` (
+  `id_request` int NOT NULL,
   `name` varchar(50) NOT NULL DEFAULT '0',
   `description` varchar(50) NOT NULL DEFAULT '0',
   `id_category` int NOT NULL DEFAULT '0',
-  `photo` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_request`),
-  KEY `FK_request_photo` (`photo`),
-  CONSTRAINT `FK_request_photo` FOREIGN KEY (`photo`) REFERENCES `photo` (`id_photo`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
+  `photo` int NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Дамп данных таблицы hakaton2022.request: ~1 rows (приблизительно)
+--
+-- Дамп данных таблицы `request`
+--
+
 INSERT INTO `request` (`id_request`, `name`, `description`, `id_category`, `photo`) VALUES
-	(1, 'asfaf', '0asfasf', 1, 1);
+(1, 'asfaf', '0asfasf', 1, 1);
 
--- Дамп структуры для таблица hakaton2022.users
-CREATE TABLE IF NOT EXISTS `users` (
-  `id_user` int NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int NOT NULL,
   `login` varchar(50) DEFAULT NULL,
   `password` varchar(50) DEFAULT NULL,
-  `name` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3;
+  `name` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Дамп данных таблицы hakaton2022.users: ~3 rows (приблизительно)
+--
+-- Дамп данных таблицы `users`
+--
+
 INSERT INTO `users` (`id_user`, `login`, `password`, `name`) VALUES
-	(1, 'admin', 'admin', 'admin'),
-	(2, 'ivan', 'zabi', 'Ivan'),
-	(7, 'abobi', '123', 'Абобович');
+(1, 'admin', 'admin', 'admin'),
+(2, 'ivan', 'zabi', 'Ivan'),
+(7, 'abobi', '123', 'Абобович');
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`);
+
+--
+-- Индексы таблицы `photo`
+--
+ALTER TABLE `photo`
+  ADD PRIMARY KEY (`id_photo`);
+
+--
+-- Индексы таблицы `problem`
+--
+ALTER TABLE `problem`
+  ADD PRIMARY KEY (`id_problem`),
+  ADD KEY `FK_problem_photo` (`id_photo`),
+  ADD KEY `FK_problem_category` (`id_category`),
+  ADD KEY `FK_problem_users` (`id_user`);
+
+--
+-- Индексы таблицы `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`id_request`),
+  ADD KEY `FK_request_photo` (`photo`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `photo`
+--
+ALTER TABLE `photo`
+  MODIFY `id_photo` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `problem`
+--
+ALTER TABLE `problem`
+  MODIFY `id_problem` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `request`
+--
+ALTER TABLE `request`
+  MODIFY `id_request` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `problem`
+--
+ALTER TABLE `problem`
+  ADD CONSTRAINT `FK_problem_category` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_problem_photo` FOREIGN KEY (`id_photo`) REFERENCES `photo` (`id_photo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `FK_problem_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ограничения внешнего ключа таблицы `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `FK_request_photo` FOREIGN KEY (`photo`) REFERENCES `photo` (`id_photo`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

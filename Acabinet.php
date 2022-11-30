c576157c9091067a71d2530c2330fc9b2b5f1cc2
Kloody <kloodyjpe@gmail.com>
2022-12-01 03:16:45 +0500

    Update Acabinet.php

--- Acabinet.php
+++ Acabinet.php
@@ -27,25 +27,25 @@ if (isset($_GET['del_id'])) { //проверяем, есть ли перемен
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 
 <body>
-	<?
+  <?
   //Вывод шапки
   require_once "template\header.php";
   ?>
 
-	<div class="container">
-    <div class="d-flex justify-content-between">
+  <div class="container">
+    <div class="d-flex justify-content-between px-5 my-4">
       <h1>Режим просмотра</h1>
-      <a class="btn btn-primary" href="/edit.php" role="button">Критерии</a>
+      <a class="btn btn-primary col-sm-4 text-center" href="/edit.php" role="button">Критерии</a>
     </div>
-		
-		<div class="table-responsive col-sm-12 col-md-9 mx-auto">
-			<?
+
+    <div class="table-responsive col-sm-12 col-md-9 mx-auto">
+      <?
       $output = $db->query('SELECT * FROM `problem` INNER JOIN category ON problem.id_category = category.id_category WHERE `status` = "новая"');
       foreach ($output as $out) {
         echo '<div class="d-flex justify-content-center">
@@ -55,26 +55,38 @@ if (isset($_GET['del_id'])) { //проверяем, есть ли перемен
                 <img src="https://kartinkof.club/uploads/posts/2022-03/1648241964_6-kartinkof-club-p-billi-kherrington-mem-7.jpg" class="img-fluid rounded-start" alt="...">
               </div>
               <div class="col-md-8">
-                <div class="card-header">
+                <div class="card-header text-center">
                   ' . $out["name_category"] . '
                 </div>
                 <div class="card-body">
-                  <h5 class="card-title">' . $out["name_problem"] . '</h5>
-                  <p class="card-text"></p>                  
-                  <p class="card-text"><small class="text-muted">' . $out["time"] . '</small></p>    
+                <div>
+                <h5 class="card-title">' . $out["name_problem"] . '</h5>
+                  <p class="card-text"></p>                
+                  <p class="card-text"><small class="text-muted">' . $out["time"] . '</small></p>  
+                </div>
+                  <select class="form-select btn btn-outline-primary rounded-0" name="'.$out["id_problem"].'">
+                    <option value="0" disabled selected>Принять или отклонить</option>
+                    <option value="1">Решена</option>
+                    <option value="2">Отклонена</option>
+                  </select>
                 </div>
               </div>
             </div>
           </div>
 </div>';
       }
+      foreach ($output as $out) {
+        if (isset($_POST[$out["id_problem"]])) {
+          mysqli_query($db, "DELETE FROM `problem` WHERE `id_category` = {$out["id_problem"]}");
+        }
+      }
       ?>
-			</tbody>
-		</div>
-	</div>
+      </tbody>
+    </div>
+  </div>
 
 
-	<?
+  <?
   require_once "template\\footer.php";
 
   ?>

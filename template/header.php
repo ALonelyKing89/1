ee26509c2542ee0b3528f51838e1bd592af7a25e
Kloody <kloodyjpe@gmail.com>
2022-12-01 02:04:17 +0500

    gfg

--- template/header.php
+++ template/header.php
@@ -11,6 +11,7 @@
 
   ?>
 
+<<<<<<< Updated upstream
   <nav class="navbar sticky-top badge-light bg-light">
     <div class="container">
       <?
@@ -24,13 +25,24 @@
       </a>';
       }
       ?>
+=======
+  <nav class="navbar sticky-top badge-light">
+    <div class="container">
+      <a class="navbar-brand" href="\">
+        <img src="logo.png" alt="" width="40">
+      </a>
+>>>>>>> Stashed changes
       <form class="d-flex btn-group" role="search">
         <?
         if (isset($_SESSION["login"])) {
           if ($_SESSION["adm"] == 1) {
             echo '<a href="Acabinet.php" class="btn btn-info text-white">Админка</a>';
           } else {
+<<<<<<< Updated upstream
             echo '<a href="Ucabinet.php" class="btn btn-info text-white">Профиль</a>';
+=======
+            echo '<a href="Ucabinet.php" class="btn btn-info text-white">Личный кабинет</a>';
+>>>>>>> Stashed changes
           }
           echo '<a href="login.php" class="btn btn-danger">Выход</a>';
         } else {

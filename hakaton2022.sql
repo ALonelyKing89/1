5eef95db30f2c1f1a88e4488696849ea9dfd7f0a
ALonelyKing89 <119292039+ALonelyKing89@users.noreply.github.com>
2022-12-01 00:33:38 +0500

    Update hakaton2022.sql

--- hakaton2022.sql
+++ hakaton2022.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 30 2022 г., 21:28
+-- Время создания: Ноя 30 2022 г., 22:33
 -- Версия сервера: 8.0.24
--- Версия PHP: 7.4.27
+-- Версия PHP: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -80,28 +80,15 @@ CREATE TABLE `problem` (
 --
 
 INSERT INTO `problem` (`id_problem`, `name_problem`, `text_problem`, `id_category`, `id_photo`, `status`, `time`, `id_user`) VALUES
-(1, 'asd', 'asd', 2, 1, 'новая', '2022-11-30 13:53:23', 7);
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `request`
---
-
-CREATE TABLE `request` (
-  `id_request` int NOT NULL,
-  `name_request` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
-  `description_request` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '0',
-  `id_category` int NOT NULL DEFAULT '0',
-  `photo_request` int NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Дамп данных таблицы `request`
---
-
-INSERT INTO `request` (`id_request`, `name_request`, `description_request`, `id_category`, `photo_request`) VALUES
-(1, 'asfaf', '0asfasf', 1, 1);
+(1, 'asd', 'asd', 2, 1, 'новая', '2022-11-30 13:53:23', 7),
+(2, 'какойто калл', 'dada', 1, 1, 'решена', '2022-11-30 19:30:11', 2),
+(3, 'фввв', 'фвыфвфв', 2, 1, 'решена', '2022-11-30 19:30:11', 1),
+(4, 'какойто калл', 'dada', 1, 1, 'решена', '2022-11-30 19:30:11', 2),
+(5, 'фввв', 'фвыфвфв', 2, 1, 'решена', '2022-11-30 19:30:11', 1),
+(6, 'вфывф', 'фуцйуйу123', 1, 1, 'новая', '2022-11-30 19:31:42', 1),
+(7, '1231', 'ыапкупыцк', 2, 1, 'решена', '2022-11-30 19:31:42', 9),
+(8, 'вфывф', 'фуцйуйу123', 1, 1, 'новая', '2022-11-30 19:31:42', 1),
+(9, '1231', 'ыапкупыцк', 2, 1, 'решена', '2022-11-30 19:31:42', 9);
 
 -- --------------------------------------------------------
 
@@ -154,13 +141,6 @@ ALTER TABLE `problem`
   ADD KEY `FK_problem_users` (`id_user`);
 
 --
--- Индексы таблицы `request`
---
-ALTER TABLE `request`
-  ADD PRIMARY KEY (`id_request`),
-  ADD KEY `FK_request_photo` (`photo_request`) USING BTREE;
-
---
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
@@ -186,13 +166,7 @@ ALTER TABLE `photo`
 -- AUTO_INCREMENT для таблицы `problem`
 --
 ALTER TABLE `problem`
-  MODIFY `id_problem` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT для таблицы `request`
---
-ALTER TABLE `request`
-  MODIFY `id_request` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_problem` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
@@ -211,12 +185,6 @@ ALTER TABLE `problem`
   ADD CONSTRAINT `FK_problem_category` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_problem_photo` FOREIGN KEY (`id_photo`) REFERENCES `photo` (`id_photo`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `FK_problem_users` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Ограничения внешнего ключа таблицы `request`
---
-ALTER TABLE `request`
-  ADD CONSTRAINT `FK_request_photo` FOREIGN KEY (`photo_request`) REFERENCES `photo` (`id_photo`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

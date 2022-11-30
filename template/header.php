fda1d78db4caf73bcf1bc7ee339d3022d7d1170d
Kloody <kloodyjpe@gmail.com>
2022-12-01 01:29:25 +0500

    Update header.php

--- template/header.php
+++ template/header.php
@@ -2,36 +2,34 @@
   $url = $_SERVER['REQUEST_URI'];
 
   $url = explode('?', $url);
-  
+
   $url = $url[0];
 
   ?>
-  
+
   <nav class="navbar sticky-top badge-light">
     <div class="container">
       <a class="navbar-brand" href="\">
-        <img src="logo.png" alt="" width="40" >
+        <img src="logo.png" alt="" width="40">
       </a>
-      <form class="d-flex" role="search">
-      <?
-        if (isset($_SESSION["login"])){
-      ?>
-          <a href="login.php" class="btn btn-info">выход</a>
-      <?     
-      }else{
-        if ($url != '/login.php'){
-      ?>          
-        <a href="login.php" class="btn btn-outline-info">вход</a>
-        <?  
+      <form class="d-flex btn-group" role="search">
+        <?
+        if (isset($_SESSION["login"])) {
+          if ($_SESSION["adm"] == 1) {
+            echo '<a href="Acabinet.php" class="btn btn-info text-white">Админка</a>';
+          } else {
+            echo '<a href="Ucabinet.php" class="btn btn-info text-white">Личный кабинет</a>';
+          }
+          echo '<a href="login.php" class="btn btn-danger">Выход</a>';
+        } else {
+          if ($url == '/register.php') {
+            echo '<a href="login.php" class="btn btn-success">Вход</a>';
+          };
+          if ($url == '/login.php') {
+            echo '<a href="register.php" class="btn btn btn-info text-white">Регистрация</a>';
+          };
         }
-        if ($url != '/register.php'){
-        ?> 
-      <a href="register.php" class="btn btn-outline-info">регистрация</a>
-      <?  
-        }
-      }
-      ?> 
+        ?>
       </form>
     </div>
   </nav>
-

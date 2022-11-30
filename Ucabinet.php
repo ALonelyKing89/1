cce0990d8123ccd87bc701a57c2e3c65834f320e
RUGBY <58521547+rimches@users.noreply.github.com>
2022-11-30 21:10:07 +0500

    11

--- Ucabinet.php
+++ Ucabinet.php
@@ -4,6 +4,7 @@ session_start();
 
 // Подключаемся к БД
 require_once 'db.php';
+$l = $db->query('SELECT * FROM request');
 //$prob = $db->query('SELECT * FROM probl');
 ?>
 <!DOCTYPE html>
@@ -21,10 +22,46 @@ require_once "template\header.php";
 
 <center><h1>USER</h1></center>
 
+<?
+/* Запрос в БД */
+/* Все варианты сортировки */
+$sort_list = array(
+	'date'       => '`date`',
+	'type'       => '`type`',
+	'food'       => '`food`',
+	'price_asc'  => '`price`',
+	'price_desc' => '`price` DESC',   
+);
+ 
+/* Проверка GET-переменной */
+$sort = @$_GET['sort'];
+if (array_key_exists($sort, $sort_list)) {
+	$sort_sql = $sort_list[$sort];
+} else {
+	$sort_sql = reset($sort_list);
+}
+ 
+/* Запрос в БД */
+$dbh = new PDO('mysql:dbname=hakaton2022;host=localhost', 'root', '');
+$sth = $dbh->prepare("SELECT * FROM `hakaton2022` ORDER BY {$sort_sql}");
+$sth->execute();
+$list = $sth->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<?
+foreach ($l as $log){
+                      echo 
+                      "<center>"."<table>"."<thead>"."<tr>"."<th>Название<th>"."<th>Описание<th>"."<th>IDN<th>"."<th>Фото<th>"."</tr>"."</thead>"."<tbody>"."<tr>"."<th>".$log["name"]."<th>"."<th>".$log["description"]."<th>"."<th>".$log["id_category"]."<th>"."<th>".$log["photo"]."<th>"."</tr>"."</tbody>"."</table>"."</center>";
+                  };
+?>
+
+
+
+
+
 
 <?
 require_once "template\\footer.php";
-
 ?>
   </body>
 </html>
